ca9049333516a71a1563546ca226e43819e44865
tsymliakov <denis.tsym@mail.ru>
2024-07-15 23:54:57 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Привет, мир"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
